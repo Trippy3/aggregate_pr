170d6008f54c7280532eb6c83152db77207d9fe0
trippy <horseheadnebula.137@gmail.com>
2023-01-24 22:42:34 +0900

    update repository.py

--- modules/repository.py
+++ modules/repository.py
@@ -1,16 +1,27 @@
+from pathlib import Path
 from dataclasses import dataclass, field, InitVar
 from urllib.parse import urlparse
+
+me = Path(__file__).resolve()
 
 
 @dataclass(frozen=True)
 class Repository:
     addr: InitVar[str]
-    # TODO: add token
+    token_file: InitVar[Path | None] = None
     owner: str = field(init=False)
     name: str = field(init=False)
+    token: str | None = field(init=False, default=None)
 
-    def __post_init__(self, addr):
+    def __post_init__(self, addr, token_file):
         # TODO: add Secured Validation
         url_path = urlparse(addr).path
         object.__setattr__(self, "owner", url_path.split("/")[1])  # path is "/owner/repo"
         object.__setattr__(self, "name", url_path.split("/")[2])
+        if token_file is None:
+            token_file = me.parents[1] / ".token"
+        if not token_file.exists():
+            print(f"Warning: Token file does not exist. path: {token_file}")
+            return
+        with open(token_file, "r") as f:
+            object.__setattr__(self, "token", f.readline())
--- modules/tests/test_repository.py
+++ modules/tests/test_repository.py
@@ -5,4 +5,5 @@ def test_RetunrsRepo_TakeAddr():
     addr = "https://github.com/Trippy3/aggregate_pr"
     repo = Repository(addr)
     assert repo.owner == "Trippy3"
-    assert repo.name ==  "aggregate_pr"
+    assert repo.name == "aggregate_pr"
+    assert repo.token is not None

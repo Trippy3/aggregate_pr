6ec69647983f09fddcfb67f10808cdd240c8ea3d
trippy <horseheadnebula.137@gmail.com>
2023-01-29 00:32:54 +0900

    fix test repository

--- modules/tests/test_repository.py
+++ modules/tests/test_repository.py
@@ -7,32 +7,33 @@ from ..repository import Repository
 ADDR = "https://github.com/Trippy3/aggregate_pr"
 
 
-def test_RetunrsRepo_TakeAddr():
-    repo = Repository(ADDR)
-    assert repo.owner == "Trippy3"
-    assert repo.name == "aggregate_pr"
-    assert repo.token is None
+class TestRepository:
+    def test_RetunrsRepo_TakeAddr(self):
+        repo = Repository(ADDR)
+        assert repo.owner == "Trippy3"
+        assert repo.name == "aggregate_pr"
+        assert repo.token is None
 
 
-token_files = [
-    (None),
-    ("./nonexistent_file"),
-    (Path("./nonexistent_file")),
-]
+    token_files = [
+        (None),
+        ("./nonexistent_file"),
+        (Path("./nonexistent_file")),
+    ]
 
 
-@pytest.mark.parametrize("token_file", token_files)
-def test_RetunrsRepo_TakeAddrAndInvalidTokenFile(token_file):
-    repo = Repository(ADDR, token_file)
-    assert repo.owner == "Trippy3"
-    assert repo.name == "aggregate_pr"
-    assert repo.token is None
+    @pytest.mark.parametrize("token_file", token_files)
+    def test_RetunrsRepo_TakeAddrAndInvalidTokenFile(self, token_file):
+        repo = Repository(ADDR, token_file)
+        assert repo.owner == "Trippy3"
+        assert repo.name == "aggregate_pr"
+        assert repo.token is None
 
 
-def test_RetunrsRepo_TakeAddrAndTokenFile(tmp_path):
-    CONTENT = "ghp_XXXXXXXXXXXXX"
-    (test_token := tmp_path / ".test_token").write_text(CONTENT)
-    repo = Repository(ADDR, test_token)
-    assert repo.owner == "Trippy3"
-    assert repo.name == "aggregate_pr"
-    assert repo.token == CONTENT
+    def test_RetunrsRepo_TakeAddrAndTokenFile(self, tmp_path):
+        CONTENT = "ghp_XXXXXXXXXXXXX"
+        (test_token := tmp_path / ".test_token").write_text(CONTENT)
+        repo = Repository(ADDR, test_token)
+        assert repo.owner == "Trippy3"
+        assert repo.name == "aggregate_pr"
+        assert repo.token == CONTENT
